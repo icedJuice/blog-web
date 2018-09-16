6f89b4215bfa7352c74f67949858fcb2b8b4174e
hanghozhi <1210712996@qq.com>
2018-09-17 00:50:10 +0800

    code lighlight

---
+++ pages/article/_id.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="container">
+      <vheader></vheader>
+      <div class="article-container">
+      <pre>
+          <code class="javascript">
+export function JAMS__mark() {
+    if (/android/i.test(navigator.userAgent)) {
+        try {
+            window.android.JAMS__mark(params)
+        } catch (e) {
+            console.log(e)
+        }
+    } else if (/ios|iphone|ipod|pad/i.test(navigator.userAgent)) {
+        try {
+            window.webkit.messageHandlers.JAMS__mark.postMessage(params)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+}
+          </code>
+        </pre>
+      </div>
+    </div>
+</template>
+
+<script>
+import vheader from '~/components/vheader.vue';
+export default {
+  name: "artilist",
+  components: {
+    vheader
+  },
+  head: {
+    script:[
+      {src: '/highlight/highlight.pack.js'},
+      {src: '/highlight/inithighlight.js', defer: 'defer'}
+    ]
+  },
+  mounted() {
+  },
+  methods: {
+
+  }
+
+}
+</script>
+
+<style scoped>
+  .article-container{
+    width: 96%;
+    max-width: 800px;
+  }
+
+</style>
