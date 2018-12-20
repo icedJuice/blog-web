4ded9acc3cf6c34e801bee356e8cfd494926caeb
hanghozhi <1210712996@qq.com>
2018-12-20 23:53:37 +0800

    add rainbow

--- pages/article/_id.vue
+++ pages/article/_id.vue
@@ -10,7 +10,7 @@
       </div>
       <div class="article-container">
       <pre>
-          <code class="javascript" data-lang="javascript">
+          <code class="javascript" data-language="JavaScript">
                 export function JAMS__mark() {
                     if (/android/i.test(navigator.userAgent)) {
                         try {
@@ -38,20 +38,31 @@ export default {
   name: "artipage",
   validate ({ params }) {
     // Must be a number
-    console.log('11111')
-    console.log(params.id)
     return /^\d+$/.test(params.id)
   },
   components: {
     vheader
   },
   head: {
+    link: [
+      {href: '/css/monokai.css'}  
+    ],
     script:[
-      {src: '/static/js/rainbow-custom.min.js', defer: 'defer'},
+    //   {src: '/js/rainbow-custom.min.js', defer: 'defer', body: true },
     ]
   },
+  mounted() {
+      setTimeout(() => {
+        if (window && window.navigator) {
+          var s = document.createElement('script');
+          s.setAttribute('src', '/js/rainbow-custom.min.js');
+          console.log(s)
+          document.getElementsByTagName('body')[0].appendChild(s);
+        }
+      }, 300)
+  },
   computed: {
-
+      
   },
   methods: {
 
